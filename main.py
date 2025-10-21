9c785ac8ef3fe1b42ff8134dff9161136f3f7793
Siarhei Kulazhanka <Siarhkul@gmail.com>
2025-10-21 07:47:43 +0200

    chore: Add streaming

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 from llama_index.llms.ollama import Ollama
-from llama_index.core import SimpleDirectoryReader, Document, VectorStoreIndex,Settings
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
+from llama_index.core.settings import Settings
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
 print('Loading documents...')
 documents = SimpleDirectoryReader(
     input_files=["./eBook-How-to-Build-a-Career-in-AI.pdf"]
 ).load_data()
-print(f"Loaded {len(documents)} documents")
+print(f"Loaded {len(documents)} document pages")
 
-# Basic RAG pipeline
-
-document = Document(text="\n\n".join([doc.text for doc in documents]))
-print(document.text[:1000])
-# print(document.text[::400])
 
 llm = Ollama(model="llama3.1", request_timeout=300.0, temperature=0.1)
 embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
@@ -30,9 +26,15 @@ query_engine = index.as_query_engine(streaming=True)
 print("Query engine created. Ready to ask questions.")
 
 
-print("\nQuerying...")
-response = query_engine.query("Give me 3 pleases of advices how to get knowledge ")
+print("\nQuerying (streaming response)...")
 
+streaming_response = query_engine.query(
+    "What is the right AI job for me?"
+)
 
 print("\nResponse:")
-print(response)
+for token in streaming_response.response_gen:
+    print(token, end="", flush=True)
+
+print()
+
